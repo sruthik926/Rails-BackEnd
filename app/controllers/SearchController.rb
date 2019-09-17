9e4b511246ea653065d8752446a76ac1ffb6c8c4
github username <github email address>
2019-09-17 18:01:08 -0400

    Search Controller added with actions

---
+++ app/controllers/SearchController.rb
@@ -0,0 +1,32 @@
+class SearchController < ActionController::API
+
+
+  def index
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+
+
+  private
+
+  def search_params
+
+  end
+
+end
