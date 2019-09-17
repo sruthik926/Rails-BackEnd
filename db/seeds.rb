04df24da336cd9bed372d78fe64e757f144a6d6a
github username <github email address>
2019-09-17 17:22:27 -0400

    created Search Model and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+search1 = Search.create(searchterm: 'phone', date: Date.today)
+search2 = Search.create(searchterm: 'soap', date: Date.today)
+search3 = Search.create(searchterm: 'mac lipstick', date: Date.today)
